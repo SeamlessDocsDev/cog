ee795084880d530633fdae35a8f109b1384af423
Matthew Titmus <matt@seamlessdocs.com>
2017-07-26 11:52:42 -0400

    Increase interactive timeout from 60 to 600

--- config/config.exs
+++ config/config.exs
@@ -68,7 +68,7 @@ config :cog, Cog.Bundle.BundleSup,
   bundle_root: Path.join([File.cwd!, "bundles"])
 
 config :cog, Cog.Command.Pipeline,
-  interactive_timeout: {60, :sec},
+  interactive_timeout: {600, :sec},
   trigger_timeout: {300, :sec}
 
 config :cog, Cog.Command.Service,
